5018fb4fa4ec620f46d76aa5da1fd8617bfc0909
desales <fransiskusdes24@gmail.com>
2025-04-26 10:18:04 +0700

    now

--- latihan/src/resources/views/components/partials/script.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- script-->
-<script src="{{ asset ('front/js/jquery.min.js') }}"></script>
-<script src="{{ asset ('front/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset ('front/js/jquery.sticky.js') }}"></script>
-<script src="{{ asset ('front/js/click-scroll.js') }}"></script>
-<script src="{{ asset ('front/js/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset ('front/js/magnific-popup-options.js') }}"></script>
-<script src="{{ asset ('front/js/custom.js') }}"></script>
-<!-- end script -->
